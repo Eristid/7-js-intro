fdace3f85261fc8689b1339c83fe734932076824
Eristid <eristida.siliute@gmail.com>
2020-11-17 12:54:54 +0200

    suma intervale

--- js/suminter.js
+++ js/suminter.js
@@ -1,19 +1,24 @@
-// 0 = 0      = 10
-// 0 = 4    = 0
-// 0 - 100   = 5050
-// 574 - 815 = didelis teigiamas skaicius
-// -50 - 50  = 0
-// -70 - 30  = santykinai mazas neigiamas sveikas
 
+// 0 - 4            = 10
+// -50 - 50         = 0
+// 0 - 100          = 5050
+// 0 - 0            = 0
+// 574 - 815        = didelis teigiamas sveikas
+// -70 - 30         = santykinai mazas neigiamas sveikas
 
+const nuo = 59;
+const iki = 59;
 
-cost nuo = -70;
-const iki = 30;
 let sum = 0;
 
-for (let i=0; i<5; i++) {
-    sum = sum + i;
-    // console.log(i, '=>', sum);
-
+if (nuo < iki) {
+    for (let i = nuo; i <= iki; i++) {
+        sum = sum + i;
+    }
+} else {
+    for (let i = iki; i <= nuo; i++) {
+        sum = sum + i;
+    }
 }
-console.log('Skaiciu intervale tarp §{nuo} ir §{iki} suma yra §{sum}');
+
+console.log(`Skaičių intervale tarp ${nuo} ir ${iki} suma yra ${sum}`);
